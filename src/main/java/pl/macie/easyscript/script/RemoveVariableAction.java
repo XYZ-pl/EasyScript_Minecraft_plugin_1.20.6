9a267951c416700f895b621e9b45a8631cb95bb3
maciek2018goat-blip <maciek2018goat@gmail.com>
2026-05-31 01:35:32 +0200

    Delete src/main/java/pl/macie/easyscript/script directory

--- src/main/java/pl/macie/easyscript/script/RemoveVariableAction.java
+++
@@ -1,9 +0,0 @@
-package pl.macie.easyscript.script.action;
-
-import pl.macie.easyscript.script.SourceLocation;
-
-public final class RemoveVariableAction extends AddVariableAction {
-    public RemoveVariableAction(String name, String amount, SourceLocation source) {
-        super(name, "-" + amount, source);
-    }
-}
